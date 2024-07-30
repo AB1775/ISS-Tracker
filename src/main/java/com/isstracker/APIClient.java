4256a724d60a14b05c7f118abbdd9ffbc63e2f91
AB1775 <66264218+AB1775@users.noreply.github.com>
2024-07-29 20:44:44 -0500

    APIClient Class
    Performs an HTTP GET request to the OpenNotify API and returns the response as a String, ensuring the connection is closed properly upon completion

---
+++ src/main/java/com/isstracker/APIClient.java
@@ -0,0 +1,37 @@
+package com.isstracker;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class APIClient {
+    public static final String apiURL = "http://api.open-notify.org/iss-now.json";
+
+    public static String getAPIResponse(String apiURL) {
+        StringBuilder response = new StringBuilder();
+        HttpURLConnection connection = null;
+
+        try {
+            final URL url = new URL(apiURL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            try (BufferedReader inputStream = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
+                String inputLine;
+
+                while ((inputLine = inputStream.readLine()) != null) {
+                    response.append(inputLine);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null) {
+                connection.disconnect();
+            }
+        }
+        return response.toString();
+    }
+}
